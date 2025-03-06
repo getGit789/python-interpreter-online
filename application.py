c0621b488ad273181f1c7ce2bde11c1fae4468f5
Damir Kranjcevic <kranjcevicdamir72@gmail.com>
2025-03-06 14:33:14 +0100

    Simplify application configuration

--- application.py
+++ application.py
@@ -1,32 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from mangum import Mangum
 from backend.app.main import app
 
 # Update the application instance name to match the EB configuration
 application = app
 
-# Add CORS middleware with specific origins
-origins = [
-    "https://getgit789.github.io",
-    "https://getgit789.github.io/python-interpreter-online/",
-    "http://localhost:5000",
-    "http://127.0.0.1:5000"
-]
-
+# Add CORS middleware
 application.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
-    expose_headers=["*"],
-    max_age=3600
+    allow_headers=["*"]
 )
-
-# Create handler for AWS Lambda
-handler = Mangum(application)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(application, host="0.0.0.0", port=80)
+    uvicorn.run(application, host="0.0.0.0", port=8000)
