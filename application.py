fdf26abf1bb76eff9e366ff18d498d76b45f13c2
Damir Kranjcevic <kranjcevicdamir72@gmail.com>
2025-03-06 13:30:01 +0100

    Update AWS configuration

--- application.py
+++ application.py
@@ -1,5 +1,9 @@
-import uvicorn
-from backend.app.main import app as application
+from mangum import Mangum
+from backend.app.main import app
+
+# Create handler for AWS Lambda
+handler = Mangum(app)
 
 if __name__ == "__main__":
-    uvicorn.run(application, host="0.0.0.0", port=8002) 
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000)
