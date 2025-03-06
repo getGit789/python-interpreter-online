b884911650351282dc31bbc1a6267761eb4d2576
Damir Kranjcevic <kranjcevicdamir72@gmail.com>
2025-03-06 15:09:20 +0100

    Update port to 5000 for AWS EB compatibility

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn application:app --bind 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker
+web: gunicorn application:app --bind 0.0.0.0:5000 -k uvicorn.workers.UvicornWorker
--- application.py
+++ application.py
@@ -27,4 +27,4 @@ app = application
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("application:app", host="0.0.0.0", port=8000)
+    uvicorn.run("application:app", host="0.0.0.0", port=5000)
