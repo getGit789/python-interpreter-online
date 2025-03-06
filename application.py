6292979696ce986b8802d43261238ead45222fe1
Damir Kranjcevic <kranjcevicdamir72@gmail.com>
2025-03-06 13:56:24 +0100

    Update CORS configuration for AWS

--- application.py
+++ application.py
@@ -1,8 +1,19 @@
+from fastapi import FastAPI
 from mangum import Mangum
 from backend.app.main import app
 
 # Update the application instance name to match the EB configuration
 application = app
+
+# Add CORS middleware
+from fastapi.middleware.cors import CORSMiddleware
+application.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Temporarily allow all origins for testing
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Create handler for AWS Lambda
 handler = Mangum(application)
