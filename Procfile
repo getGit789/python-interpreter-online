8989ab995f89ac76192d131c62c037f3c2a956f5
Damir Kranjcevic <kranjcevicdamir72@gmail.com>
2025-03-06 15:02:13 +0100

    Simplify application configuration for AWS EB

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn application:application --bind 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker
+web: gunicorn application:app --bind 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker
--- application.py
+++ application.py
@@ -22,6 +22,9 @@ application.include_router(app.router)
 def health_check():
     return {"status": "healthy"}
 
+# AWS EB looks for an 'application' callable by default
+app = application
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(application, host="0.0.0.0", port=8000)
+    uvicorn.run("application:app", host="0.0.0.0", port=8000)
